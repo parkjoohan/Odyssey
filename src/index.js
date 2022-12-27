635737a2c079816977905cef49ad776b31d5965b
alex0317@naver.com <joohanpark1513@gmail.com>
2022-12-27 15:25:38 +0900

    chore: 초기 설정 변경

--- src/index.js
+++ src/index.js
@@ -3,19 +3,15 @@ import { createRoot } from 'react-dom/client';
 import { Provider } from 'react-redux';
 import App from './App';
 import './index.css';
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { store } from './app/store';
 
 const container = document.getElementById('root');
 const root = createRoot(container);
-const queryClient = new QueryClient();
 
 root.render(
   <>
-    <QueryClientProvider client={queryClient}>
-      <Provider store={store}>
-        <App />
-      </Provider>
-    </QueryClientProvider>
+    <Provider store={store}>
+      <App />
+    </Provider>
   </>
 );
